abf0a82a113757cdcc47d7a0c06fafe5cc2e6680
SrishtiKhosla <154043416+02siri@users.noreply.github.com>
2025-10-03 16:06:19 +1000

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,33 +3,19 @@ const express = require ('express');
 //Creating new (instance of) application of express 
 const app = express();
 
-//Request handler
+app.use("/user",
+    //Route Handler 1
+    (req,res, next)=>{
+        console.log("First RH")
+        res.send("First response");
+        next();
+        },
 
-    //this will only handle GET api calls to /user (/user, /user/xyz, /user/1...)
-app.get("/user", (req,res) => {
-    console.log(req.query);
-    res.send("Testing req.query");
-});
-
-app.get("/user/:userId", (req,res) => {
-    console.log(req.params.userId);
-    res.send("Testing req.params");
-});
-
-
-//the ? means, the letter before it is optional
-//so this will work for: /abc, /ac
-app.get(/^\/ab?c$/, (req,res) => {
-    res.send("Testing the ? in the route");
-});
-
-
-    //this will match all the HTTP method API calls to /test
-app.use("/test",(req,res)=>{
-    res.send("Hello from the test request");
-});
-
-
+    (req,res)=>{
+        console.log("Second RH")
+        res.send("Second response");
+    }
+    );
 
 //created a server on port 3000, and my app is listening on this server
     //this callback function will only work if my sever has been started successfully
